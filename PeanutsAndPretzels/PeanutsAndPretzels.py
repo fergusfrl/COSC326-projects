56dec49e13552427f6ab8a3b8c3c568f115f4df5
Fergus Farrell <fergusfrl@gmail.com>
2017-05-25 19:54:18 +1200

    Update PeanutsAndPretzels.py

--- PeanutsAndPretzels/PeanutsAndPretzels.py
+++ PeanutsAndPretzels/PeanutsAndPretzels.py
@@ -1,20 +1,34 @@
 # Fergus Farrell
 # Etude 4 - Peanuts and Pretzels
 
+def printtable(table):
+    for x in table:
+        print x
+
+def generatetable(size, moves):
+    table = []
+    for i in range(size):
+        table.append(False)
+
+    for i in range(len(moves)):
+        for j in range(moves[i] - 1, size, moves[i] * 2):
+            table[j] = True
+    return table
+
 moves = []
-winningHash = {}
-winningList = []
 moves.append(1)
 moves.append(2)
 moves.append(3)  # legal moves
-pileSize = 12  # number of peanuts
+peanutSize = 12  # number of peanuts
+pretzelSize = 12
+peanutwinning = generatetable(peanutSize, moves)
+pretzelwinning = generatetable(pretzelSize, moves)
+print 'Peanut Table:'
+printtable(peanutwinning)
+print '\nPretzel Table:'
+printtable(pretzelwinning)
 
-for i in range(pileSize):
-    winningList.append(False)
-    
-for i in range(len(moves)):
-    for j in range(moves[i]-1, pileSize, moves[i]*2):
-        winningList[j] = True
-
-for x in winningList:
-    print x
+if peanutwinning[len(peanutwinning)-1] or pretzelwinning[len(pretzelwinning)-1]:
+    print '\n0 0'
+else:
+    print '\nIt is posible to win. How? Shhhhh.'
